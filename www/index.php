fae1ed1845f2758cd617db02431c27a9efcde24b
Dina Polousova <dinapolousova@gmail.com>
2023-07-05 08:15:24 +0300

    lessons 3-6

--- www/index.php
+++ www/index.php
@@ -1,1 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Пример встраивания PHP в HTML</title>
+</head>
+<body>
+    
+</body>
+    <h1>Пример встраивания PHP в HTML</h1>
+    2 + 2 = <?php echo 2 + 2; ?>
+    <?php 
+        $var = 123;
+        echo "Значение переменной: $var";
+    ?>
+</html>
+
+
