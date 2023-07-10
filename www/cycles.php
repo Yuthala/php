e70d0b03e1a80b5f7a9a4d2f6623071dcbaee44d
Dina Polousova <dinapolousova@gmail.com>
2023-07-10 14:49:14 +0300

    average speed

--- www/cycles.php
+++ www/cycles.php
@@ -5,3 +5,15 @@ $carsSpeed = [
     140,
     78
 ];
+
+$sumOfSpeeds = 0;
+
+foreach ($carsSpeed as $speed) {
+    $sumOfSpeeds += $speed;
+}
+
+$countOfCars = count($carsSpeed);
+
+$averageSpeed = $sumOfSpeeds/$countOfCars;
+
+echo "Средняя скорость движения по трассе: " . $averageSpeed;
