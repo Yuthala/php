b9aed46f166fb4b5c774af18fb9a1149c68e1d5a
Dina Polousova <dinapolousova@gmail.com>
2023-07-21 14:04:20 +0300

    calculator index

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
 //$password = !empty($_GET['password']) ? $_GET['password'] : 'пароль не передан';
 //?>
 
-<!doctype html>
+<!--<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -23,6 +23,29 @@
         </label>
         <br>
         <input type="submit" value="Войти">
+    </form>
+</body>
+</html>-->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Калькулятор</title>
+</head>
+<body>
+    <form action="/result.php">
+        <input type="text" name="x1">
+        <select name="operation">
+            <option value="+">+</option>
+            <option value="-">-</option>
+        </select>
+
+        <input type="text" name="x2">
+        <input type="submit" value="Посчитать">
     </form>
 </body>
 </html>
