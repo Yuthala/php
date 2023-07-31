8f168ddb8266998bfe90754dab00dd4a0bf58b1e
Dina Polousova <dinapolousova@gmail.com>
2023-08-01 01:54:09 +0300

    fputs

--- www/files.php
+++ www/files.php
@@ -6,3 +6,11 @@ while (!feof($file)) {
     echo '<br>';
 }
 fclose($file);
+?>
+
+<?php
+$file = fopen(__DIR__ . '/file2.txt', 'w');
+for($i = 1; $i < 101; $i++) {
+    fputs($file, $i . PHP_EOL);
+}
+fclose($file);
