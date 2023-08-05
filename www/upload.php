bd9b9dc6fb32a6bb9f45acd0683d7435527b9699
Dina Polousova <dinapolousova@gmail.com>
2023-08-05 21:52:01 +0300

    extensions validation

--- www/upload.php
+++ www/upload.php
@@ -10,7 +10,13 @@ if (!empty($_FILES['attachment'])) {
     $srcFileName = $file['name'];
     $newFilePath = __DIR__ . '/uploads/' . $srcFileName;
 
-    if (file_exists($newFilePath)) {
+    $allowedExtensions = ['jpg', 'png', 'gif'];
+    $extension = pathinfo($srcFileName, PATHINFO_EXTENSION);
+    if (!in_array($extension, $allowedExtensions)) {
+        $error = 'Загрузка файлов с таким расширением запрещена!';
+    } if ($file['error'] !== UPLOAD_ERR_OK) {
+        $error = 'Ошибка при загрузке файла';
+    } elseif (file_exists($newFilePath)) {
         $error = 'Файл с таким именем уже существует';
     } elseif (!move_uploaded_file($file['tmp_name'], $newFilePath)) {
         $error = 'Ошибка при загрузке файла';
